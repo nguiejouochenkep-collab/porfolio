1ed4ac27fab76c6bfda006123cfa4e3ba8b17d91
nguiejouochenkep-collab <nguiejouochenkep@gmail.com>
2026-03-28 13:24:08 +0100

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -2,20 +2,16 @@ import React from 'react';
 import Header from '../components/Header';
 import Hero from '../components/Hero';
 import Skills from '../components/Skills';
-// import ProjectsGrid from '../components/ProjectsGrid'; // À décommenter si ce composant existe
 import Footer from '../components/Footer';
 
 const HomePage = () => {
-    // Load projects data here
-
     return (
-        <div>
+        <main className="min-h-screen bg-white">
             <Header />
             <Hero />
             <Skills />
-            {/* <ProjectsGrid /> */}
             <Footer />
-        </div>
+        </main>
     );
 };
 
