3ec35279787deded2d426a45967681bd11dd8445
nguiejouochenkep-collab <nguiejouochenkep@gmail.com>
2026-03-28 16:03:05 +0100

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import Header from '@/components/Header';
-import Hero from '@/components/Hero';
-import Skills from '@/components/Skills';
-import ProjectCard from '@/components/ProjectCard'; 
-import Footer from '@/components/Footer';
+import Header from '@/components/header';
+import Hero from '@/components/hero';
+import Skills from '@/components/skills';
+import ProjectCard from '@/components/projectcard'; 
+import Footer from '@/components/footer';
 
 const HomePage = () => {
     return (
