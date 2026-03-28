553742eac878dc5dd1821af3660f6c7742fc4062
nguiejouochenkep-collab <nguiejouochenkep@gmail.com>
2026-03-28 16:15:22 +0100

    Update projectcard.js

--- src/components/projectcard.js
+++ src/components/projectcard.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-const projectCard = ({ project }) => {
+const ProjectCard = ({ project }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
